681d9a7935526767c96a9b06eb807efee2eabe0c
Florian Colly <florian@distantia.ca>
2022-02-22 10:46:19 -0500

    redirect sans warning + add chema result

--- imports/ui/EditContact.jsx
+++ imports/ui/EditContact.jsx
@@ -4,6 +4,7 @@ import { bridge as schema } from "../apollo/contactSchema";
 import { useQuery, useLazyQuery,  gql, useMutation } from "@apollo/client";
 import {useParams} from "react-router-dom";
 import {refetch} from "./Contact.jsx"
+import { useNavigate } from "react-router-dom";
 const GET_CONTACT = gql`
 query GetContact ($id: ID!){
 	getContact (_id : $id){
@@ -34,71 +35,10 @@ const UPDATE_CONTACT = gql`
 		}
 	}
 `;
-/*
-const [updateContact, { dataUpdate, loadingUpdate, errorUpdate }] = useMutation(UPDATE_CONTACT);
-const { loading, error, data } =  useQuery (GET_CONTACT, {variables : {id : this.state.id}});
-
-export class EditContact extends Component{
-	constructor (){
-		super();
-		this.sended = this.sended.bind(this);
-		this.seeMessage = this.seeMessage.bind(this);
-		this.setId = this.setId.bind(this);
-		this.updateContact = this.updateContact.bind(this);
-		this.state = {
-			id: 0,
-			msg : "",
-			update : updateContact
-		}
-	}
-
-	componentDidMount() {
-		let  id  = useParams().id;
-		this.setId(id);
-	}
-
-	sended = () =>{
-		this.setState({msg : "Le contact a été mis à jour."});
-	}
-	seeMessage = () =>{
-		this.setState({msg : "Le contact a été mis à jour."});
-	}
-
-	setId = (newId) => {
-		this.setState({id : newId});
-	}
-
-
-	render (){
-
-		return(
-			<div>
-				<AutoForm
-				ref={ref => {
-					fRef= ref;
-				  }}
-				placeholder={true}
-				schema={schema}
-				model={data?.getContact?data.getContact : []}
-				onSubmit={(value) =>  {
-					this.updateContact({ variables : { id: id, input : value}});
-					this.sended();
-				}}
-				/>
-				<p className="formResult" onClick={seeMessage}>
-					{send}
-				</p>
-			</div>
-			);
-	}
-
-
-}
-*/
 
 export const EditContact = () => {
 
-
+		const navigate = useNavigate();
 		const [send, setSended] = useState("");
 
 		const  id  = useParams().id;
@@ -109,13 +49,17 @@ export const EditContact = () => {
 		const seeMessage = () =>{
 			setSended("");
 		}
-
+		const dataNotFound=0;
 		const {loading, error, data} =  useQuery (GET_CONTACT, {variables : {id : id}});
 
-		useEffect(() => {
-		}, []);
+
+		useEffect( async () => {
+			if(error!=undefined)
+				navigate("/error-404");
+		}, [error]);
 
 		const [updateContact, { dataUpdate, loadingUpdate, errorUpdate }] = useMutation(UPDATE_CONTACT);
+
 		return (
 			<div>
 				<AutoForm
